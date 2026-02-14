abf5ffbc542ae76a8a2c9ed421a779b8cceb6868
SantaDrop <team556coin@gmail.com>
2026-02-14 15:01:45 -0800

    version 0.1.76

--- server/priv/repo/migrations/20260214213732_add_missing_org_admin_fields.exs
+++ server/priv/repo/migrations/20260214213732_add_missing_org_admin_fields.exs
@@ -1,12 +1,7 @@
 defmodule ClippsterServer.Repo.Migrations.AddMissingOrgAdminFields do
   use Ecto.Migration
 
+  # All columns already added by 20260214000000_add_org_subscription_admin_fields
   def change do
-    alter table(:organizations) do
-      add :admin_price_cents, :integer
-      add :admin_billing_cycle_day, :integer
-      add :created_by_admin_id, :integer
-      add :setup_completed, :boolean, default: true
-    end
   end
 end
